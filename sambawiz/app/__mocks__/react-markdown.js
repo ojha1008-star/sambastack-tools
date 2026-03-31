59e53e56d7951530fe569925f8895dab8085bc48
durgesho <durgesh.ojha@sambanovasystems.com>
2026-03-31 20:52:06 +0530

    Delete sambawiz directory

--- sambawiz/app/__mocks__/react-markdown.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
-const React = require('react');
-
-const ReactMarkdown = ({ children }) => {
-  return React.createElement('div', null, children);
-};
-
-module.exports = ReactMarkdown;
